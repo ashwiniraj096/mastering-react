d81ca563dfaa1dcb605f2ba1b9fd43c5266b6fe0
Ashwini R <ashwiniraj096@gmai.com>
2023-04-15 02:13:41 +0530

    parcel, react, reactdom added

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { class: "heading" }, "h1 tag"),
-    React.createElement("h1", { class: "heading" }, "h1 tag"),
+    React.createElement("h1", { class: "heading" }, "hhhhhhhh1 tag"),
   ]),
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { class: "heading" }, "h1 tag"),
