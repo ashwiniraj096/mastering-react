3cc3c49be708c1df330d96ab9edc07fa520f52e9
Ashwini R <ashwiniraj096@gmai.com>
2024-07-05 14:09:20 +0530

    check commit

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,31 +4,29 @@ import RestrauntCard from "./RestaurantCard";
 const Body = ({ restrautList }) => {
   const [listOfRestaurants, setRestaurantList] = useState(restrautList);
   return (
-    <>
-      <div className="body">
-        <div className="filter">
-          <button
-            className="filter-btn"
-            onClick={() => {
-              const filteredList = restrautList.filter(
-                (res) => res.data.avgRating >= 4
-              );
-              setRestaurantList(filteredList);
-              console.log(filteredList);
-            }}
-          >
-            Top Rated Restaurants
-          </button>
-        </div>
-        <div className="restaurant-list">
-          {listOfRestaurants.map((restaurant) => {
-            return (
-              <RestrauntCard {...restaurant.data} key={restaurant.data.id} />
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = restrautList.filter(
+              (res) => res.data.avgRating >= 4
             );
-          })}
-        </div>
+            setRestaurantList(filteredList);
+            console.log(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
-    </>
+      <div className="restaurant-list">
+        {listOfRestaurants.map((restaurant) => {
+          return (
+            <RestrauntCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
